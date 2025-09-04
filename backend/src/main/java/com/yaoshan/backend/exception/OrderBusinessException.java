fd319bd014ba495d199b136bbcdb79f3e95a97a9
hjt <2658342293@qq.com>
2025-09-04 18:23:43 +0800

    删除无用文件

--- backend/src/main/java/com/yaoshan/backend/exception/OrderBusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.yaoshan.backend.exception;
-
-public class OrderBusinessException extends BaseException {
-
-    public OrderBusinessException(String msg) {
-        super(msg);
-    }
-
-}
--- backend/src/main/java/com/yaoshan/backend/exception/ShoppingCartBusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.yaoshan.backend.exception;
-
-public class ShoppingCartBusinessException extends BaseException {
-
-    public ShoppingCartBusinessException(String msg) {
-        super(msg);
-    }
-
-}
